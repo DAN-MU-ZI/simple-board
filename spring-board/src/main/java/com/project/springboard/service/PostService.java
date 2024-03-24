74b52bb43162effb7098acb5bdacce078d411997
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-03-25 05:05:59 +0900

    #7 - feat - 게시판 서비스 및 비영속화 DTO 구현
    1. 게시글 작성, 수정, 삭제, 목록 조회의 기능을 구현함
    
    2. PostDTO 를 구현해서 탈영속화 적용

---
+++ spring-board/src/main/java/com/project/springboard/service/PostService.java
@@ -0,0 +1,61 @@
+package com.project.springboard.service;
+
+import com.project.springboard.domain.Post.Post;
+import com.project.springboard.domain.Post.PostDTO;
+import com.project.springboard.domain.User.User;
+import com.project.springboard.repository.PostRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post savePost(final User user, final PostDTO postDTO) {
+        Post post = Post.builder()
+                .user(user)
+                .title(postDTO.getTitle())
+                .content(postDTO.getContent())
+                .build();
+
+        return postRepository.save(post);
+    }
+
+    public PostDTO updatePost(final PostDTO postDTO) {
+        Post existingPost = getExistingPost(postDTO);
+
+        Post updatedPost = Post.builder()
+                .id(existingPost.getId())
+                .user(existingPost.getUser())
+                .title(postDTO.getTitle())
+                .content(postDTO.getContent())
+                .build();
+
+        postRepository.save(updatedPost);
+
+        return PostDTO.from(updatedPost);
+    }
+
+    public void deletePost(final PostDTO postDTO) {
+        Post existingPost = getExistingPost(postDTO);
+
+        postRepository.delete(existingPost);
+    }
+
+    private Post getExistingPost(final PostDTO postDTO) {
+        return postRepository.findById(postDTO.getId())
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 Post가 없습니다. id=" + postDTO.getId()));
+    }
+
+    public List<PostDTO> findAllPosts() {
+
+        List<Post> posts = postRepository.findAll();
+
+        return posts.stream()
+                .map(PostDTO::from)
+                .collect(Collectors.toList());
+    }
+}
