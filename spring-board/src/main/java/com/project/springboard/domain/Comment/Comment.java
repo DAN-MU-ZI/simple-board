814155a9addb717825fbd923af6f8751521f5dda
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-03-28 13:56:45 +0900

    #6 - feat - Comment 엔티티, 레포지토리 구현 및 연관관계 설정
    Comment <-> Post 양방향 매핑

--- spring-board/src/main/java/com/project/springboard/domain/Comment/Comment.java
+++ spring-board/src/main/java/com/project/springboard/domain/Comment/Comment.java
@@ -1,47 +1,45 @@
-package com.project.springboard.domain.Post;
+package com.project.springboard.domain.Comment;
 
-import com.project.springboard.domain.Comment.Comment;
+import com.project.springboard.domain.Post.Post;
 import com.project.springboard.domain.User.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Table
-@Entity
-public class Post {
+public class Comment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String title;
+	@Column(nullable = false)
 	private String content;
 
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
 
-	@OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
-	private final List<Comment> comments = new ArrayList<>();
+	@ManyToOne
+	@JoinColumn(name = "post_id")
+	private Post post;
 
 	@Builder
-	public Post(final Long id, final String title, final String content, final User user) {
+	public Comment(final Long id, final String content, final User user, final Post post) {
 		this.id = id;
-		this.title = title;
 		this.content = content;
 		this.user = user;
+		this.post = post;
 	}
 }
