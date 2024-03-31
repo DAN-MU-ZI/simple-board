65d00c7aeb9b793c17504e7f1bd12b5695f90c53
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-03-31 14:15:40 +0900

    #6 - feat - 사용자 기반 댓글 조회 기능 추가

--- spring-board/src/main/java/com/project/springboard/repository/PostRepository.java
+++ spring-board/src/main/java/com/project/springboard/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.project.springboard.repository;
 
 import com.project.springboard.domain.Post.Post;
+import com.project.springboard.domain.User.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+	List<Post> findAllByUser(User user);
 }
