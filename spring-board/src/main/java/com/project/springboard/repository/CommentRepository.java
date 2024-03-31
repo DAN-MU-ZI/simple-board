907794e67fff1f4b865963cf6efee42ced0e4a56
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-03-31 14:18:58 +0900

    #6 - feat - 댓글 레포 기능 추가

--- spring-board/src/main/java/com/project/springboard/repository/CommentRepository.java
+++ spring-board/src/main/java/com/project/springboard/repository/CommentRepository.java
@@ -1,9 +1,12 @@
 package com.project.springboard.repository;
 
 import com.project.springboard.domain.Comment.Comment;
+import com.project.springboard.domain.User.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+	List<Comment> findAllByUser(User user);
 }
